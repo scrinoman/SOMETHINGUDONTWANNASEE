fe429f076c67c33029dafb1d4561eeed634ba024
RoadToExclusivity <dimaoq@gmail.com>
2015-12-26 13:13:32 +0300

    Need merge to master

--- lex_dll/lex_dll.h
+++
@@ -1,5 +0,0 @@
-#ifdef LEX_DLL_EXPORTS
-#define LEX_DLL_API __declspec(dllexport)
-#else
-#define LEX_DLL_API __declspec(dllimport)
-#endif
--- syntax_dll/symbol_export.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#ifdef SYNTAX_DLL_EXPORTS
-#define SYNTAX_DLL_API __declspec(dllexport)
-#else
-#define SYNTAX_DLL_API __declspec(dllimport)
-#endif
